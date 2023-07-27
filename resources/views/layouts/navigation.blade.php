c325353bb1ccf438e9184e6593ab01471f1758b4
sabaaa96 <saba.mzd@gmail.com>
2023-07-27 08:23:58 +0200

    Initial commit

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,19 @@
+{{--Navigation for desktop--}}
+<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        {{ __('Dashboard') }}
+    </x-nav-link>
+    <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+        {{ __('Chirps') }}
+    </x-nav-link>
+</div>
+
+{{--Navigation for mobile--}}
+<div class="pt-2 pb-3 space-y-1">
+    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        {{ __('Dashboard') }}
+    </x-responsive-nav-link>
+    <x-responsive-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+        {{ __('Chirps') }}
+    </x-responsive-nav-link>
+</div>
